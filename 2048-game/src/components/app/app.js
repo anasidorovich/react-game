64b7900c54a362be8fd92cf4743dde7458dbc729
Anastasiya Sidarovich <anastasiya.sidarovich@digitech7.by>
2021-03-03 12:20:41 +0300

    fix: update styles to put footer on the bottom

--- 2048-game/src/components/app/app.js
+++ 2048-game/src/components/app/app.js
@@ -351,75 +351,77 @@ function App() {
 
   return (
     <div className={`app mr-auto ml-auto ${theme}`}>
-      <Header
-        onChangeTheme={setTheme}
-        onSizeSelect={onChangeGridSize}
-        gridSize={gridSize}
-        currentTheme={currentTheme}
-      />
-      <Route path="/about" component={AboutPage} exact />
-      <Route path="/statistics" component={StatsPage} exact />
-      <Route path="/" exact>
-        <GameHeading
-          score={state.score}
-          bestScore={bestScore}
-          onClickNewGame={onClickNewGame}
-          onClickAutoPlay={onClickAutoPlay}
-          onClickOptions={onClickOptions}
-          playable={playable}
+      <div className="page-wrapper">
+        <Header
+          onChangeTheme={setTheme}
+          onSizeSelect={onChangeGridSize}
+          gridSize={gridSize}
+          currentTheme={currentTheme}
         />
-        <div className="fullscreen">
-          <div
-            {...swipeHandlers}
-            className="game-container wrapper bg-primary text-uppercase mt-3 mb-5"
-          >
-            <button
-              type="button"
-              className="btn fullscreen-btn btn-primary"
-              onClick={onFullScreenChange}
+        <Route path="/about" component={AboutPage} exact />
+        <Route path="/statistics" component={StatsPage} exact />
+        <Route path="/" exact>
+          <GameHeading
+            score={state.score}
+            bestScore={bestScore}
+            onClickNewGame={onClickNewGame}
+            onClickAutoPlay={onClickAutoPlay}
+            onClickOptions={onClickOptions}
+            playable={playable}
+          />
+          <div className="fullscreen">
+            <div
+              {...swipeHandlers}
+              className="game-container wrapper bg-primary text-uppercase mt-3 mb-5"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-fullscreen"
-                viewBox="0 0 16 16"
+              <button
+                type="button"
+                className="btn fullscreen-btn btn-primary"
+                onClick={onFullScreenChange}
               >
-                <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z"></path>
-              </svg>
-            </button>
-            <GridContainer data={data} size={tileSize} />
-            <GridItemContainer items={state.tiles} size={tileSize} />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-fullscreen"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z"></path>
+                </svg>
+              </button>
+              <GridContainer data={data} size={tileSize} />
+              <GridItemContainer items={state.tiles} size={tileSize} />
+            </div>
+            {showOptions && (
+              <OptionsPopup
+                show={showOptions}
+                soundIsChecked={soundIsChecked}
+                musicIsChecked={musicIsChecked}
+                onClickClose={onCloseOptions}
+                gridSize={gridSize}
+                onChangeGridSize={onChangeGridSize}
+                difficultyNum={difficultyNum}
+                onChangeLevel={onChangeLevel}
+                onChangeSound={onChangeSound}
+                onChangeMusic={onChangeMusic}
+                soundsVolume={soundsVolume}
+                musicVolume={musicVolume}
+                onChangeSoundsVolume={onChangeSoundsVolume}
+                onChangeMusicVolume={onChangeMusicVolume}
+              />
+            )}
+            {(state.hasWon || state.gameOver) && (
+              <Popup
+                theme={theme}
+                show={true}
+                onHide={onHidePopup}
+                popup={popup}
+              />
+            )}
           </div>
-          {showOptions && (
-            <OptionsPopup
-              show={showOptions}
-              soundIsChecked={soundIsChecked}
-              musicIsChecked={musicIsChecked}
-              onClickClose={onCloseOptions}
-              gridSize={gridSize}
-              onChangeGridSize={onChangeGridSize}
-              difficultyNum={difficultyNum}
-              onChangeLevel={onChangeLevel}
-              onChangeSound={onChangeSound}
-              onChangeMusic={onChangeMusic}
-              soundsVolume={soundsVolume}
-              musicVolume={musicVolume}
-              onChangeSoundsVolume={onChangeSoundsVolume}
-              onChangeMusicVolume={onChangeMusicVolume}
-            />
-          )}
-          {(state.hasWon || state.gameOver) && (
-            <Popup
-              theme={theme}
-              show={true}
-              onHide={onHidePopup}
-              popup={popup}
-            />
-          )}
-        </div>
-      </Route>
+        </Route>
+      </div>
       <Footer />
     </div>
   );
